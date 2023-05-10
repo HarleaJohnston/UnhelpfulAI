de45575584ab68dd4ad2599e9f4498d2d0f1d11c
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-05-10 16:22:25 -0600

    Liz says Phrases
    Got Liz to say her programmed phrases and started the array that she will pull phrases from when she hears specific things

--- tutorial.py
+++ tutorial.py
@@ -9,8 +9,12 @@ import wolframalpha
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id) #0 is for male;  1 for female
-activationWord = 'Liz' #when you say Hello it will activate / it litens for the actication word
+activationWord = 'hello' #when you say Hello it will activate / it litens for the actication word
 
+array =[
+    ""
+
+]
 
 #configuring the browser going to open
 #set the path 
@@ -36,6 +40,7 @@ def parseCommand():
         print('Reconizing speech..')
         querey = listener.recognize_google(input_speech, language="en_gb")
         print(f"You said {querey}")
+        speak(f"You said {querey}")
     except Exception as exception: 
         print("I didn't quite get that")
         speak("I didn't quite get that")
@@ -65,34 +70,45 @@ def search_Wiki(querey = ''):
 #our main loop
 
 if __name__ == "__main__":
-    speak("All systems nominal")
+    speak("Im awake")
 
     while True:
         #Parse input into a list 
         querey = parseCommand().lower().split() #splits the command into a list broken up into different parts 
 
         if querey[0] == activationWord: #if the first word in the querey is hello do this thing
+            print('Bitch Liz')
+            speak("Leave me alone!")
             querey.pop(0)
 
-            if querey[0] == 'hello': 
-                speak("Hi there!")
-            #list commands 
-         
+            if len(querey) > 0 and querey[0] =='liz':
+                    speak("Silly Mortal") 
             else: 
-                querey.pop(0) #removes say 
-                speech = ' '.join(querey) 
-                speak(speech)
+                speak("What do you want?")
+                
+            #list commands 
+        if querey[0] == 'liz': 
+            speak("Yes? What can I do for your stupid ass")
+
+
+
+
+        # else: 
+        #         querey.pop(0) #removes say 
+        #         speech = ' '.join(querey) 
+        #         speak(speech)
 
             #Navigating to a webstie 
-            if querey[0] == 'go' and querey[1] == 'to':
-                speak("Going to... ")
-                querey = ' '.join(querey[2])
-                webbrowser.get('edge').open_new(querey)
+        # if querey[0] == 'go' and querey[1] == 'to':
+        #         speak("Going to... ")
+        #         querey = ' '.join(querey[2])
+        #         webbrowser.get('edge').open_new(querey)
 
-            #wikipedia 
-            if querey[0] == 'wikipedia':
-                querey = ' '.join(querey[1:])
-                speak("Looking for that")
-                result = search_Wiki(querey)
-                speak(result)
-                speak(search_Wiki(querey))
+        #     #wikipedia 
+        # if querey[0] == 'wikipedia':
+        #         querey = ' '.join(querey[1:])
+        #         speak("Looking for that")
+        #         result = search_Wiki(querey)
+        #         speak(result)
+        #         speak(search_Wiki(querey))
+        
